eb4a65240cbb93d457154662ff16440d271d3737
Juan Camilo Renza Potes <juancamilo.renza@gmail.com>
2020-04-25 16:12:02 -0500

    Movimiento del Personaje

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
 {
 	private CharacterController characterController;
 	private Animator animator;
-	public float x,y;
 
 	[SerializeField]
+	private float crouchSpeed = 50f;
 	private float moveSpeed = 100f;
+	private float runSpeed = 200f;
+	private int movementType;
 	[SerializeField]
-	private float turnSpeed = 5f;
+	private float turnSpeed = 100f;
 
 	private void Awake()
 	{
@@ -23,17 +26,20 @@ public class PlayerMovement : MonoBehaviour
 	{
 		var horizontal = Input.GetAxis("Horizontal");
 		var vertical = Input.GetAxis("Vertical");
-
 		var movement = new Vector3(horizontal, 0, vertical);
 
-		characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+
+
+		if (Input.GetKey(KeyCode.LeftShift)) { characterController.SimpleMove(movement * Time.deltaTime * runSpeed); movementType = 3; }
+		else if (Input.GetKey(KeyCode.LeftControl)) { characterController.SimpleMove(movement * Time.deltaTime * crouchSpeed); movementType = 2; }
+		else { characterController.SimpleMove(movement * Time.deltaTime * moveSpeed); movementType = 1; }
 
 		animator.SetFloat("Speed", movement.magnitude);
+		animator.SetInteger("Type", movementType);
 
 		if (movement.magnitude > 0)
 		{
 			Quaternion newDirection = Quaternion.LookRotation(movement);
-
 			transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
 		}
 
