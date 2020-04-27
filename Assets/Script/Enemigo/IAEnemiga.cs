825855b314450c51d22dc2b921a3da27557dbcb6
JuanFrood <juan_f.rodriguez_c@uao.edu.co>
2020-04-27 17:55:00 -0500

    Just Alive 1.0
    IA del enemigo

---
+++ Assets/Script/Enemigo/IAEnemiga.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAEnemiga : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject Target;
+    [SerializeField]
+    public NavMeshAgent agent;
+    [SerializeField]
+    public float distance;
+
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(Target.transform.position, transform.position) < distance)
+        {
+
+            agent.SetDestination(Target.transform.position);
+            agent.speed = 5;
+
+        }
+        else
+        {
+            agent.speed = 0;
+
+        }
+
+
+
+
+        
+    }
+}
