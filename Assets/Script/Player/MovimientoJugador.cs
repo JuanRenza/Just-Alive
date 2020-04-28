3f20bfb2ac77139c92f2afbc853b44b8e64e73b4
Juan Camilo Renza Potes <juancamilo.renza@gmail.com>
2020-04-27 21:01:13 -0500

    Salud y Daño
    El jugador al colisionar con el enemigo pierde una cantidad especifica de vida (Falta realizar la animación)

---
+++ Assets/Script/Player/MovimientoJugador.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+	private CharacterController jugador;
+	private Animator animator;
+	private Rigidbody rb;
+
+	[SerializeField]
+	public float moveSpeed;
+	private int movementType;
+	[SerializeField]
+	private float turnSpeed = 100f;
+	private Transform camara;
+
+	private void Start() {
+		rb = GetComponent<Rigidbody>();
+		jugador = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		var horizontal = Input.GetAxis("Horizontal");
+		var vertical = Input.GetAxis("Vertical");
+		var movJugador = new Vector3(horizontal , 0, vertical);
+
+
+		if (Input.GetKey(KeyCode.LeftShift)) {
+			movementType = 3;
+			moveSpeed = 200f;
+		}
+		else if (Input.GetKey(KeyCode.LeftControl)) { 
+			movementType = 2;
+			moveSpeed = 50f;
+		}
+		else {
+			movementType = 1;
+			moveSpeed = 100f;
+		}
+
+		jugador.SimpleMove(movJugador * Time.deltaTime * moveSpeed);
+
+		animator.SetFloat("Speed", movJugador.magnitude);
+		animator.SetInteger("Type", movementType);
+
+		if (movJugador.magnitude > 0)
+		{
+			Quaternion newDirection = Quaternion.LookRotation(movJugador);
+			transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+		}
+
+	}
+
+}
---
+++ Assets/Script/Player/VidaDaño.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class VidaDaño : MonoBehaviour
+{
+	private Animator animator;
+	public int vida = 100;
+	public bool invencible = false;
+	public float tiempo_invencible = 1f;
+	public float tiempo_frenado = 0.2f;
+
+	private void start(){
+		animator = GetComponent<Animator>();
+	}
+
+
+	public void restarVida(int cantidad) 
+	{
+		if(!invencible && vida > 0)
+		{
+			vida -= cantidad;
+			StartCoroutine(Invulnerabilidad());
+			StartCoroutine(FrenarVelocidad());
+		}
+	}
+
+	IEnumerator Invulnerabilidad()
+	{
+		invencible = true;
+		yield return new WaitForSeconds(tiempo_invencible);
+		invencible = false;
+	}
+
+		IEnumerator FrenarVelocidad()
+	{
+		var velocidadactual = GetComponent<MovimientoJugador>().moveSpeed;
+		GetComponent<MovimientoJugador>().moveSpeed = 0;
+		yield return new WaitForSeconds(tiempo_frenado);
+		GetComponent<MovimientoJugador>().moveSpeed = velocidadactual;
+	}
+}
