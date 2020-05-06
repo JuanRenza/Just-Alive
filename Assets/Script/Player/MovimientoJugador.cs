0e70ec2200dbb1bb781bd153919cb0c2084c4f51
Juan Camilo Renza Potes <juancamilo.renza@gmail.com>
2020-05-06 16:08:24 -0500

    Cambio del tipo de movimiento
    Se realiza el cambio del tipo de movimiento y del seguimiento de la camara, no se realiza la implementación total, solo parcial

--- Assets/Script/Player/MovimientoJugador.cs
+++ Assets/Script/Player/MovimientoJugador.cs
@@ -1,65 +1,111 @@
-﻿using System;
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using UnityEngine;
+
+// This script moves the character controller forward
+// and sideways based on the arrow keys.
+// It also jumps when pressing space.
+// Make sure to attach a character controller to the same game object.
+// It is recommended that you make only one call to Move or SimpleMove per frame.
 
 public class MovimientoJugador : MonoBehaviour
 {
-	private CharacterController jugador;
+    CharacterController jugador;
 	private Animator animator;
 	private Rigidbody rb;
+	private Camera mainCamera;
 
-	[SerializeField]
-	public float moveSpeed;
+    public float speed;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
 	public int movementType;
+	public float moveSpeed;
 	[SerializeField]
-	private float turnSpeed = 100f;
-	private Transform camara;
+	public float sensibilidadCamara = 1.0f;
 
-	private void Start() {
-		rb = GetComponent<Rigidbody>();
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
 		jugador = GetComponent<CharacterController>();
 		animator = GetComponent<Animator>();
-	}
+		mainCamera = Camera.main;
 
-	private void Update()
-	{
-		var horizontal = Input.GetAxis("Horizontal");
-		var vertical = Input.GetAxis("Vertical");
-		var movJugador = new Vector3(horizontal , 0, vertical);
+		Cursor.lockState = CursorLockMode.Locked;
+    }
 
+    void Update()
+    {
+		movimiento();
+		movimientoCamara();
+
+		if(Input.GetKeyDown(KeyCode.Escape)){Cursor.lockState=CursorLockMode.None;}
+		
+    }
+
+	void Paso(){
+        FindObjectOfType<AudioManager>().Play("Paso");
+    }
+
+	void movimiento(){
+		
 		//Animacion para morir
 		if (movementType==4){animator.Play("Muerte");}
-		jugador.SimpleMove(movJugador * Time.deltaTime * moveSpeed);
 		//Movimiento al correr
 		if (Input.GetKey(KeyCode.LeftShift)) {
 			movementType = 3;
-			moveSpeed = 120f;
+			speed = 7.0f;
 		}
 		//Movimiento al agacharse
 		else if (Input.GetKey(KeyCode.LeftControl)) { 
 			movementType = 2;
-			moveSpeed = 40f;
+			speed = 2.5f;
 		}
 		//Movimiento al caminar
-		else if (horizontal != 0 || vertical != 0){
+		else {
 			movementType = 1;
-			moveSpeed = 50f;
+			speed = 3.0f;
 		}
 
-		animator.SetFloat("Speed", movJugador.magnitude);
+        if (jugador.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+		
+		animator.SetFloat("Speed", moveDirection.magnitude);
 		animator.SetInteger("Type", movementType);
 
-		if (movJugador.magnitude > 0)
-		{
-			Quaternion newDirection = Quaternion.LookRotation(movJugador);
-			transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
-		}
+        moveDirection.y -= gravity * Time.deltaTime;
+
+		//Posicionar la direcion del movimiento a la del juego
+		moveDirection = transform.TransformDirection(moveDirection);
+        jugador.Move(moveDirection * Time.deltaTime);
 		movementType = 0;
 	}
 
-    void Paso(){
-        FindObjectOfType<AudioManager>().Play("Paso");
-    }
+	void movimientoCamara(){
+
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+
+		//Mirar a los lados
+		Vector3 rotacionY = transform.localEulerAngles;
+		rotacionY.y += mouseX * sensibilidadCamara;
+		transform.localRotation = Quaternion.AngleAxis(rotacionY.y,Vector3.up);
+
+		//Mirar arriba y abajo
+		Vector3 rotacionX = mainCamera.gameObject.transform.localEulerAngles;
+		rotacionX.x -= mouseY * sensibilidadCamara;
+		mainCamera.gameObject.transform.localRotation = Quaternion.AngleAxis(rotacionX.x,Vector3.right);
+
+	}
 }
